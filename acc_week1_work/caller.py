384f6ab7138bdd422dbcba3a7101ddc9e74756aa
suma619 <36477909+suma619@users.noreply.github.com>
2019-12-15 15:02:39 +0530

    Add files via upload

---
+++ acc_week1_work/caller.py
@@ -0,0 +1,10 @@
+import my_module
+k=my_module.sum(6,7)
+print(k)
+
+age=my_module.person["age"]
+print(age)
+
+for i in my_module.person:
+    print(i)
+    print(my_module.person[i])
---
+++ acc_week1_work/my_module.py
@@ -0,0 +1,22 @@
+def sum(a,b):
+    return a+b
+
+person={"name":'suma',
+        "age":12,
+        "salary":10000
+        }
+
+
+json_mod1='{"name":"mahesh",
+            "age":30,
+            "salary":20000
+           }'
+
+k2={"name":'suma',
+        "age":12,
+        "salary":10000
+        }
+
+
+def mul(a,b):
+    return a*b
